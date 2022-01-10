e7810b3c335a13b19d19809831350fe8af04c9e3
DK <spol@naver.com>
2022-01-10 19:52:55 +0900

    게임 셋팅 및 화면 설정

---
+++ TetrisGame/Background/BrickGenertor.swift
@@ -0,0 +1,31 @@
+//
+//  BrickGenertor.swift
+//  TetrisGame
+//
+//  Created by DK on 2022/01/10.
+//
+
+import Foundation
+import SpriteKit
+class BrickGenerator {
+    
+    let brickValue = Variables.brickValue
+    init(){
+        let brick = brickValue.points
+        for item in brick{
+            let x = Int(item.x) + Variables.dx
+            let y = Int(item.y) + Variables.dy
+            Variables.backarrays[y][x] = 1
+            let xValue = x * brickValue.brickSize + Int(Variables.startPoint.x)
+            let yValue = y * brickValue.brickSize + Int(Variables.startPoint.y)
+            let brick = SKSpriteNode()
+            brick.color = brickValue.color
+            brick.size = CGSize(width: brickValue.brickSize - Variables.gab, height: brickValue.brickSize - Variables.gab)
+            brick.name = brickValue.brickName
+            brick.zPosition = brickValue.zPosition
+            brick.position = CGPoint(x: xValue, y: -yValue)
+            Variables.scene.addChild(brick)
+        }
+    }
+}
+
--- TetrisGame/GameScene.swift
+++ TetrisGame/GameScene.swift
@@ -11,8 +11,9 @@ import GameplayKit
 class GameScene: SKScene {
 
     override func didMove(to view: SKView) {
-        
+        Variables.scene = self
         let bg = BackGround()
+        let generator = BrickGenerator() 
         checkBrick()
     }
     
